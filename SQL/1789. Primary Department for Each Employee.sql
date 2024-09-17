4a55396ba2d439f8168b56a10ab5d264de6bd9ae
Nikita Zvekov <cooper30@mail.ru>
2024-09-17 16:33:04 +0400

    Leetcode

---
+++ SQL/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,90 @@
+/*
+Table: Employee
++---------------+---------+
+| Column Name   |  Type   |
++---------------+---------+
+| employee_id   | int     |
+| department_id | int     |
+| primary_flag  | varchar |
++---------------+---------+
+(employee_id, department_id) is the primary key (combination of columns with unique values) for this table.
+employee_id is the id of the employee.
+department_id is the id of the department to which the employee belongs.
+primary_flag is an ENUM (category) of type ('Y', 'N'). If the flag is 'Y', the department is the primary department
+for the employee. If the flag is 'N', the department is not the primary.
+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+employee_id is the column with unique values for this table.
+This table contains information about the employees and the id of the manager they report to. Some employees do not
+report to anyone (reports_to is null).
+
+Employees can belong to multiple departments. When the employee joins other departments, they need to decide which
+department is their primary department. Note that when an employee belongs to only one department, their primary
+column is 'N'.
+Write a solution to report all the employees with their primary department. For employees who belong to one department,
+report their only department.
+Return the result table in any order.
+The result format is in the following example.
+
+Example 1:
+
+Input:
+Employee table:
++-------------+---------------+--------------+
+| employee_id | department_id | primary_flag |
++-------------+---------------+--------------+
+| 1           | 1             | N            |
+| 2           | 1             | Y            |
+| 2           | 2             | N            |
+| 3           | 3             | N            |
+| 4           | 2             | N            |
+| 4           | 3             | Y            |
+| 4           | 4             | N            |
++-------------+---------------+--------------+
+
+Employees table:
++-------------+---------+------------+-----+
+| employee_id | name    | reports_to | age |
++-------------+---------+------------+-----+
+| 9           | Hercy   | null       | 43  |
+| 6           | Alice   | 9          | 41  |
+| 4           | Bob     | 9          | 36  |
+| 2           | Winston | null       | 37  |
++-------------+---------+------------+-----+
+Output:
++-------------+---------------+
+| employee_id | department_id |
++-------------+---------------+
+| 1           | 1             |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
++-------------+---------------+
+
++-------------+-------+---------------+-------------+
+| employee_id | name  | reports_count | average_age |
++-------------+-------+---------------+-------------+
+| 9           | Hercy | 2             | 39          |
++-------------+-------+---------------+-------------+
+Explanation:
+- The Primary department for employee 1 is 1.
+- The Primary department for employee 2 is 1.
+- The Primary department for employee 3 is 3.
+- The Primary department for employee 4 is 3.
+*/
+SELECT d.employee_id,
+       d.department_id
+FROM Employee d
+WHERE d.primary_flag = 'Y'
+      OR d.primary_flag = 'N' AND NOT EXISTS(
+            SELECT NULL
+            FROM Employee
+            WHERE employee_id = d.employee_id
+                  AND department_id != d.department_id
+         );
